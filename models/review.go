29b2d9169babf50b1fb92e38cbd1cd1ce15d1fe5
Rafaela Cavalcante <pcavalcante@fanatics.com>
2022-10-21 21:09:57 -0300

    add doula and midwife

--- models/review.go
+++ models/review.go
@@ -14,11 +14,14 @@ type Review struct {
 	City       int       `json:"city"`
 	State      int       `json:"state"`
 	Place      string    `json:"place"`
-	PlaceRate  int       `json:"placerate"`
+	PlaceRate  int       `json:"place_rate"`
 	Doctor     string    `json:"doctor"`
-	DoctorRate int       `json:"doctorrate"`
+	DoctorRate int       `json:"doctor_rate"`
+	Doula      string    `json:"doula"`
+	DoulaRate  int       `json:"doula_rate"`
 	Team       string    `json:"team"`
-	TeamRate   int       `json:"teamrate"`
+	TeamRate   int       `json:"team_rate"`
 	UpdatedAt  time.Time `json:"updated_at"`
 	CreatedAt  time.Time `json:"created_at"`
+	Image      string    `json:"image"`
 }
