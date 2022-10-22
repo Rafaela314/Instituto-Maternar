dca8709836826ca9e6457980135a4ed38214b029
Rafaela Cavalcante <pcavalcante@fanatics.com>
2022-10-21 21:10:48 -0300

    add doula and midwife to review

--- models/review.go
+++ models/review.go
@@ -6,22 +6,25 @@ import (
 
 //Review represent review model
 type Review struct {
-	ID         int       `json:"id"`
-	Username   string    `json:"username"`
-	Title      string    `json:"title" validate:"required"`
-	Content    string    `json:"content" validate:"required"`
-	Date       string    `json:"date"`
-	City       int       `json:"city"`
-	State      int       `json:"state"`
-	Place      string    `json:"place"`
-	PlaceRate  int       `json:"place_rate"`
-	Doctor     string    `json:"doctor"`
-	DoctorRate int       `json:"doctor_rate"`
-	Doula      string    `json:"doula"`
-	DoulaRate  int       `json:"doula_rate"`
-	Team       string    `json:"team"`
-	TeamRate   int       `json:"team_rate"`
-	UpdatedAt  time.Time `json:"updated_at"`
-	CreatedAt  time.Time `json:"created_at"`
-	Image      string    `json:"image"`
+	ID          int       `json:"id"`
+	Username    string    `json:"username"`
+	Title       string    `json:"title" validate:"required"`
+	Content     string    `json:"content" validate:"required"`
+	Date        string    `json:"date"`
+	City        string    `json:"city"`
+	State       string    `json:"state"`
+	Country     string    `json:"country"`
+	Place       string    `json:"place"`
+	PlaceRate   int       `json:"place_rate"`
+	Doctor      string    `json:"doctor"`
+	DoctorRate  int       `json:"doctor_rate"`
+	MidWife     string    `json:"midwife"`
+	MidWifeRate int       `json:"midwife_rate"`
+	Doula       string    `json:"doula"`
+	DoulaRate   int       `json:"doula_rate"`
+	Team        string    `json:"team"`
+	TeamRate    int       `json:"team_rate"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	CreatedAt   time.Time `json:"created_at"`
+	Image       string    `json:"image"`
 }
