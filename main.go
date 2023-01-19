0e3b2736731e5e25d2f24b7e916814f4c305d779
Rafaela Cavalcante <pcavalcante@fanatics.com>
2023-01-19 14:58:45 -0300

    add server on the start up

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "git.com/lib/pq"
+	"github.com/Rafaela314/instituto-maternar/api"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/imdb?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("could not connect to db:", err)
+	}
+	fmt.Println(conn.Driver())
+
+	server := api.NewServer()
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("could not start server")
+	}
+
 }
