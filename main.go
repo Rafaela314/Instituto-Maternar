d15681637187a3a11c17e1022dfcc88d6b095c86
Rafaela Cavalcante <pcavalcante@fanatics.com>
2023-01-19 16:42:11 -0300

    get config from viper

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
-	_ "git.com/lib/pq"
+	//_ "git.com/lib/pq"
 	"github.com/Rafaela314/instituto-maternar/api"
 	"github.com/Rafaela314/instituto-maternar/util"
 )
@@ -14,13 +16,13 @@ func main() {
 	if err != nil {
 		log.Fatal("could not load config:", err)
 	}
-	/*
-		conn, err := sql.Open(config.DBDriver, config.DBSource)
-		if err != nil {
-			log.Fatal("could not connect to db:", err)
-		}
-		fmt.Println(conn.Driver())
-	*/
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("could not connect to db:", err)
+	}
+	fmt.Println(conn.Driver())
+
 	server := api.NewServer()
 
 	err = server.Start(config.ServerAddress)
